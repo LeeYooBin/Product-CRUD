e39917e5e7d490b89b4cc89546d7de4800fddb30
LeeYooBin <santosroberdan25@gmail.com>
2024-06-26 23:15:17 -0300

    Initializing Spring boot and setting initial configs

---
+++ products-register/src/main/java/com/products_register/ProductsRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.products_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsRegisterApplication.class, args);
+	}
+
+}
